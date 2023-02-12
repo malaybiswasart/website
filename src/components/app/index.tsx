b94061b1f16f028da4f9bf047eae89c9ce5f66c5
mbadeveloper <biswas.malay@gmail.com>
2023-02-11 19:15:21 -0600

    Initial add

---
+++ src/components/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+  return (
+    <div>
+      <img src="media/logo/logo-1080X1080.png"></img>
+      <div>Coming soon</div>
+    </div>
+  );
+}
+
+export default App;
